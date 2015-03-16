5753ceb7bd2bc1c6143a755308af48cc6fbeaa3a
MendezOscar <oscarito_16m@live.com.mx>
2015-03-16 16:38:38 -0600

    Log Entry

---
+++ logEntryFilter/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using logEntryFilter.Models;
+
+namespace logEntryFilter.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View(new RegisterModel());
+        }
+        [Register]
+        [HttpPost]
+
+        public ActionResult Register(RegisterModel modelo)
+        {
+            if (modelo.Password != modelo.ConfirmPassword)
+                throw new Exception("Contraseñas No coinciden");
+
+            return View(modelo);
+
+        }
+    }
+}
---
+++ logEntryFilter/RegisterAttribute.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using log4net;
+using log4net.Repository.Hierarchy;
+using WebGrease;
+
+namespace logEntryFilter
+{
+    public class RegisterAttribute : FilterAttribute, IActionFilter, IResultFilter, IExceptionFilter
+    {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+        public void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+          Logger.Info(filterContext.ActionDescriptor.ActionName.ToString()); 
+                 
+        }
+
+        public void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+           
+        }
+
+        public void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+        
+        }
+
+        public void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+            
+        }
+
+        public void OnException(ExceptionContext filterContext)
+        {
+         Logger.ErrorException(filterContext.Exception.Message.ToString(),filterContext.Exception); 
+        }
+    {
+    }
+}
